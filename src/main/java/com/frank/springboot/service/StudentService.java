85f09f99a2d3a0d262b629c6647c7f1fb5d4ee4a
Frank yang <yangkun@sefon.com>
2021-11-01 16:19:10 +0800

    清除测试数据

--- src/main/java/com/frank/springboot/service/StudentService.java
+++ src/main/java/com/frank/springboot/service/StudentService.java
@@ -1,8 +1,7 @@
 package com.frank.springboot.service;
 
 import com.frank.springboot.model.Student;
-//2021-11-01 15:31:47提交测试
-//2021-11-01 15:50:05提交测试2
+
 public interface StudentService {
     /**
      * 根据学生id查询详情
